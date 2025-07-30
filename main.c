8d9e7eee9d263dc08a98c923127b31fda33acb1a
brunohro <brunoh.rodrigues27@gmail.com>
2025-07-30 13:24:57 -0300

    Atualizando a função mostrar caminho

--- main.c
+++ main.c
@@ -195,23 +195,31 @@ void imprimir_matriz_int(int M[MAX][MAX])
     }
 }
 
-void mostrar_caminho(int origem, int destino){
-    if(D[origem][destino] >= INF){
+void mostrar_caminho(int origem, int destino)
+{
+    if (D[origem][destino] >= INF)
+    {
         printf("Nao ha caminho disponivel entre os municipios.\n");
         return;
     }
 
     int caminho[MAX], tam = 0;
     int atual = destino;
-    while(atual != origem){
+    while (atual != origem)
+    {
         caminho[tam++] = atual;
         atual = P[origem][atual];
     }
     caminho[tam++] = origem;
 
     printf("Caminho:    ");
-    for(int i = tam - 1; i >= 0; i--){
+    for (int i = tam - 1; i >= 0; i--)
+    {
         printf("%s", nomes[caminho[i]]);
+        if (i > 0)
+        {
+            printf(" -> ");
+        }
+        printf("\nCusto total: %.2f\n", D[origem][destino]);
     }
-
 }
